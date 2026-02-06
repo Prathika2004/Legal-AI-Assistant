9e217eee56ee22e50756e3d794d7b91bbfd3ae98
Prathika M <prathimuthu04@gmail.com>
2026-02-06 21:13:41 +0530

    fix: simplify nlp engine and update requirements

--- core/nlp_engine.py
+++ core/nlp_engine.py
@@ -1,17 +1,9 @@
 import spacy
-import os
 
 class NLPEngine:
     def __init__(self):
-        model_name = "en_core_web_sm"
-        try:
-            # Try to load the model
-            self.nlp = spacy.load(model_name)
-        except OSError:
-            # If model not found, download it automatically (Fix for Streamlit Cloud)
-            import spacy.cli
-            spacy.cli.download(model_name)
-            self.nlp = spacy.load(model_name)
+        
+        self.nlp = spacy.load("en_core_web_sm")
 
     def get_basic_entities(self, text):
         doc = self.nlp(text[:100000])
